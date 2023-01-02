43b26ce0ab136bfc3fafd85c32a8e185769bf217
swethamallavarapu <swethamallavarapu15@gmail.com>
2023-01-02 19:10:12 +0530

    centella-vite

--- src/Layout/DefaultLayout.jsx
+++ src/Layout/DefaultLayout.jsx
@@ -1,11 +1,7 @@
-import React from "react"
+import React from "react";
 class Layout extends React.Component {
-  render(){
-    return (
-      <>
-                
-      </>
-    )
+  render() {
+    return <></>;
   }
 }
 export default Layout;
