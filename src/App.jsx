cc6e638a8c70db996d224c2c986cc2a7ffa7b3f7
swethamallavarapu <swethamallavarapu15@gmail.com>
2023-01-03 14:36:13 +0530

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import "./App.css";
 import AuthPage from "./pages/Authpage";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import CentChem from "./pages/CentChem";
 function App() {
   return (
-    <div className="App">
-      <AuthPage></AuthPage>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <main>
+          <Routes>
+            <Route path="/">
+              <AuthPage />
+            </Route>
+            <Route path="/CentChem">
+              <CentChem />
+            </Route>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/CentChem.jsx
@@ -0,0 +1,4 @@
+const CentChem = () => {
+  return <div>Hello</div>;
+};
+export default CentChem;
